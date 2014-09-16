5fd664cd66fc7677b8dda607d4df5e55658edead
Jeremy Proffitt <jeremy@mobiusnm.com>
2014-09-16 00:47:23 -0400

    updated Site commands and added user commands. Modified for use with civicrm-buildkit

---
+++ src/Joomlatools/Console/Command/UserAbstract.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		Mozilla Public License, version 2.0
+ * @link		http://github.com/joomlatools/joomla-console for the canonical source repository
+ */
+
+namespace Joomlatools\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Joomlatools\Console\Joomla\Bootstrapper;
+
+abstract class UserAbstract extends Command
+{
+
+    protected $target_dir;
+    protected $userParams;
+    protected $groups;    
+
+    protected function configure()
+    {
+        $this->addOption(
+            'base',
+            'b',
+            InputOption::VALUE_OPTIONAL,
+            'Base directory for the Joomla Installation. Default is the current directory'
+        )
+        ->addOption(
+            'name',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'First and Last name of this user'
+        )
+        ->addOption(
+            'user',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Alphanumeric user email'
+        )
+        ->addOption(
+            'pass',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Password for the user'
+        )
+        ->addOption(
+            'email',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Email address for this user'
+        )
+        ->addOption(
+            'group',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'User group to assign to this user. Choose admin or registered. Default is registered.',
+            'registered'
+        )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->target_dir = $input->getOption('base');
+        
+        $this->app = Bootstrapper::getApplication($this->target_dir);
+
+        $name = $input->getOption('name');
+        $user = $input->getOption('user');
+        $email = $input->getOption('email');
+        $pass = $input->getOption('pass');
+
+        if (empty($this->userParams->pass)) 
+        {
+            require_once JPATH_BASE.'/libraries/joomla/user/helper.php';
+            $pass = \JUserHelper::genRandomPassword(14);
+        }
+
+        $groupname = $input->getOption('group');
+        $this->groups = $this->getGroups();
+
+        $group = $this->groups[$groupname];
+
+        $this->userParams = (object) array('name'=>$name,'user'=>$user,'pass'=>$pass,'email'=>$email,'group'=>$group->id);
+        
+    }
+
+    protected function getGroups()
+    {
+        // Get the list of user groups
+        $jDb = $this->app->getDbo();
+        $userGroupQuery =  $jDb->getQuery(true);
+        $userGroupQuery
+          ->select(array('id','lower(title) as title'))
+          ->from('#__usergroups');
+        $jDb->setQuery($userGroupQuery);
+        $accessGroupList = $jDb->loadObjectList('title');
+
+        return $accessGroupList;
+    }
+
+
+
+
+}
---
+++ src/Joomlatools/Console/Command/UserCreate.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		Mozilla Public License, version 2.0
+ * @link		http://github.com/joomlatools/joomla-console for the canonical source repository
+ */
+
+namespace Joomlatools\Console\Command;
+
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UserCreate extends UserAbstract
+{
+    
+
+    protected function configure()
+    {
+        parent::configure();
+        $this
+            ->setName('user:create')
+            ->setDescription('Create a Joomla user');
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::execute($input, $output);
+        
+        $this->createUser($this->userParams);
+        
+    }
+
+ /**
+   * Function to create a user of Joomla.
+   *
+   * @param array  $params associated array
+   * @param string $mail email id for cms user
+   *
+   * @return uid if user exists, false otherwise
+   *
+   * @access public
+   */
+  public function createUser(&$params) {
+
+    $baseDir = JPATH_BASE;
+    require_once $baseDir . '/components/com_users/models/registration.php';
+
+    $userParams = \JComponentHelper::getParams('com_users');
+    $model      = new \UsersModelRegistration();
+
+    // check for a specified usertype, else get the default usertype
+    if (isset($params->group)) $userType = $params->group;
+    else {
+      $userType = $userParams->get('new_usertype');
+      if (!$userType) {
+        $userType = 2;
+      }
+    }
+
+    $fullname = trim($params->name);
+
+    // Prepare the values for a new Joomla user.
+    $values              = array();
+    $values['name']      = $fullname;
+    $values['username']  = trim($params->name);
+    $values['password1'] = $values['password2'] = $params->pass;
+    $values['email1']    = $values['email2'] = trim($params->email);
+    $values['groups']    = array($userType);
+
+    // $lang = $this->app->getLanguage();;
+    // $lang->load('com_users', $baseDir);
+
+    $register = $model->register($values);
+
+    $uid = \JUserHelper::getUserId($values['username']);
+    return $uid;
+  }
+    
+
+
+}
